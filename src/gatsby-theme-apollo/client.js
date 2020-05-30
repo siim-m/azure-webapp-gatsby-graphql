fcd40fe5e17a1c7a4dcef4450be831b43132fda6
Siim Männart <46551195+siim-m@users.noreply.github.com>
2020-05-30 21:30:09 +1000

    fixes typo in api url

--- src/gatsby-theme-apollo/client.js
+++ src/gatsby-theme-apollo/client.js
@@ -4,7 +4,7 @@ import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-boost';
 const client = new ApolloClient({
   cache: new InMemoryCache(),
   link: new HttpLink({
-    uri: 'https://salmon-forest-0f5115203.azurestaticapps.net/api/grapql',
+    uri: 'https://salmon-forest-0f5115203.azurestaticapps.net/api/graphql',
     fetch
   })
 });
